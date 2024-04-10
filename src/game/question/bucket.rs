0b8e1fbbf224b9fb016312c6f4381473bd09bcd1
Kitsai <luck3801@gmail.com>
2024-04-09 23:39:26 -0300

    bucket impl

--- src/game/question/bucket.rs
+++ src/game/question/bucket.rs
@@ -12,3 +12,40 @@ pub struct Bucket {
     _base_bucket: Vec<u8>,
     _current_bucket: Vec<u8>
 }
+
+impl Bucket {
+    pub fn new(bucket_type: BucketTypes) -> Self {
+        Bucket {
+            bucket_type,
+            _base_bucket: Vec::new(),
+            _current_bucket: Vec::new()
+        }
+    }
+
+    pub fn get_bucket_type(&mut self, bucket_type: &BucketTypes) -> Vec<u8> {
+        match bucket_type {
+            BucketTypes::Single => {
+                vec![0,1,2,3,4,5]
+            },
+            BucketTypes::Multiple(size) => {
+                let ret = Vec::new();
+                for _ in 0..*size {
+                    self._base_bucket.append(&mut vec![0,1,2,3,4,5]);
+                }
+                ret
+            },
+            BucketTypes::Incremental(size) => {
+                let ret = Vec::new();
+                for i in 0..*size {
+                    self._base_bucket.push((i as u8) % 6);
+                }
+                ret
+            },
+            BucketTypes::Custom(custom) => {
+                custom.clone()
+            }
+        }
+    }
+
+    
+}
